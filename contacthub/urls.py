2767e8db469ebd62b664ab0609384abb8e28cf5e
Malcom-Yingwani <nsovomalcom@gmail.com>
2025-12-08 19:39:19 +0200

    Once of management command

--- contacthub/urls.py
+++ contacthub/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('contacts.urls'))
-]
+
+urlpatterns = [path("admin/", admin.site.urls), path("", include("contacts.urls"))]
+
+
+def create_admin(request):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@example.com", "YourPassword123")
+        return HttpResponse("Admin created!")
+    return HttpResponse("Admin already exists")
