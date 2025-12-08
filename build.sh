4b6009d6b2d045a2954b1ffeb48eecb459fb34c4
Malcom-Yingwani <nsovomalcom@gmail.com>
2025-12-08 20:42:14 +0200

    fixed python manage.py createsuperuser

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-pythom manage.py createsuperuser --noinput || true
+python manage.py createsuperuser --noinput || true
