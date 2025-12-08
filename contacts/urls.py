338464773b0281e2d44d171d6673a6e5f5ff778f
Malcom-Yingwani <nsovomalcom@gmail.com>
2025-12-08 21:45:44 +0200

    download document changes

--- contacts/urls.py
+++ contacts/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("search/", views.search_contacts, name="search"),
     path("create/", views.create_contact, name="create-contact"),
     path("delete<int:pk>/", views.delete_contact, name="delete-contact"),
+    path(
+        "contacts/<int:pk>/download/", views.download_document, name="download-document"
+    ),
 ]
